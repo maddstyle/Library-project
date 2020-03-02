9609149e8ec815eea0516aff81edf8f14361988e
Michaela DeCamara <michaela.decamara@gmail.com>
2020-03-02 15:10:52 -0500

    author added

--- library-project/routes/book/add.js
+++ library-project/routes/book/add.js
@@ -1,13 +1,44 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-
-router.get('/books/add', (req, res, next) => {
+router.get("/books/add", (req, res, next) => {
   res.render("book-add");
 });
 
-router.post('/books/add', (req, res, next) => {
-  
+router.post("/books/add", (req, res, next) => {
+  const { title, author, description, rating } = req.body;
+  const newBook = new Book({ title, author, description, rating });
+  newBook
+    .save()
+    .then(book => {
+      res.redirect('/books');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.get("/authors/add", (req, res, next) => {
+  res.render("author-add");
+});
+
+router.post("/authors/add", (req, res, next) => {
+  const { name, lastName, nationality, birthday, pictureUrl } = req.body;
+  const newAuthor = new Author({
+    name,
+    lastName,
+    nationality,
+    birthday,
+    pictureUrl
+  });
+  newAuthor
+    .save()
+    .then(book => {
+      res.redirect("/books");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
--- library-project/routes/index.js
+++ library-project/routes/index.js
@@ -1,64 +1,66 @@
-const express = require('express');
-const router  = express.Router();
-const Book = require('../models/book.js'); 
+const express = require("express");
+const router = express.Router();
+const Book = require("../models/book.js");
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/books', (req, res, next) => {
+router.get("/books", (req, res, next) => {
   Book.find()
-  .then(allTheBooksFromDB => {
-    console.log('Retrieved books from DB:', allTheBooksFromDB);
-    res.render('books', { books: allTheBooksFromDB });
-  })
-  .catch(error => {
-    console.log('Error while getting the books from the DB: ', error);
-  })
-res.render('books');
-});
-
-router.get('/books/:bookId', (req, res, next) => {
-  console.log('The ID from the URL is: ', bookId);
-  res.render('book-details');
-});
-
-router.get('/books/:bookId', (req, res, next) => {
-  Book.findById(req.params.bookId)
-  .then(theBook => {
-    res.render('book-details', { book: theBook });
-  })
-    .catch(error => {
-      console.log('Error while retrieving book details: ', error);
+    .then(allTheBooksFromDB => {
+      console.log("Retrieved books from DB:", allTheBooksFromDB);
+      res.render("books", { books: allTheBooksFromDB });
     })
+    .catch(error => {
+      console.log("Error while getting the books from the DB: ", error);
+    });
+  res.render("books");
 });
 
-router.get('/books/add', (req, res, next) => {
+router.get("/books/:bookId", (req, res, next) => {
+  console.log("The ID from the URL is: ", bookId);
+  res.render("book-details");
+});
+
+router.get("/books/:bookId", (req, res, next) => {
+  Book.findById(req.params.bookId)
+    .then(theBook => {
+      res.render("book-details", { book: theBook });
+    })
+    .catch(error => {
+      console.log("Error while retrieving book details: ", error);
+    });
+});
+
+router.get("/books/add", (req, res, next) => {
   res.render("book-add");
 });
 
-router.post('/books/add', (req, res, next) => {
+router.post("/books/add", (req, res, next) => {
   const { title, author, description, rating } = req.body;
-  const newBook = new Book({ title, author, description, rating});
-  newBook.save()
-.then((book) => {
-  res.redirect('/books');
-})
-.catch((error) => {
-
-})
+  const newBook = new Book({ title, author, description, rating });
+  newBook
+    .save()
+    .then(book => {
+      res.redirect("/books");
+    })
+    .catch(error => {});
 });
 
-router.post('/books/edit', (req, res, next) => {
+router.post("/books/edit", (req, res, next) => {
   const { title, author, description, rating } = req.body;
-  Book.update({_id: req.query.book_id}, { $set: {title, author, description, rating }}, { new: true })
-  .then((book) => {
-    res.redirect('/books');
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+  Book.update(
+    { _id: req.query.book_id },
+    { $set: { title, author, description, rating } },
+    { new: true }
+  )
+    .then(book => {
+      res.redirect("/books");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
-
 
 module.exports = router;
